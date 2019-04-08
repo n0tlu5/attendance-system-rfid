62cecb5da35cee36661b27dfc4472349d8c9cf9a
Darfi Sultoni <darfisultoni@gmail.com>
2019-04-09 01:48:33 +0700

    minor

--- webapp/AJKehadiran.sql
+++ webapp/AJKehadiran.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Server version:               5.7.23-0ubuntu0.18.04.1 - (Ubuntu)
+-- Server version:               5.7.25-0ubuntu0.18.04.2 - (Ubuntu)
 -- Server OS:                    Linux
 -- HeidiSQL Version:             9.5.0.5196
 -- --------------------------------------------------------
@@ -12,25 +12,14 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for test
-CREATE DATABASE IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `test`;
-
--- Dumping structure for table test.kehadiran
-CREATE TABLE IF NOT EXISTS `kehadiran` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `student_id` varchar(14) NOT NULL,
-  `class_id` int(11) DEFAULT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `FK_kehadiran_kelas` (`class_id`),
-  KEY `FK_kehadiran_mahasiswa` (`student_id`),
-  CONSTRAINT `FK_kehadiran_kelas` FOREIGN KEY (`class_id`) REFERENCES `kelas` (`id`),
-  CONSTRAINT `FK_kehadiran_mahasiswa` FOREIGN KEY (`student_id`) REFERENCES `mahasiswa` (`nrp`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
+-- Dumping database structure for AJKehadiran
+DROP DATABASE IF EXISTS `AJKehadiran`;
+CREATE DATABASE IF NOT EXISTS `AJKehadiran` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `AJKehadiran`;
 
 -- Data exporting was unselected.
--- Dumping structure for table test.kelas
+-- Dumping structure for table AJKehadiran.kelas
+DROP TABLE IF EXISTS `kelas`;
 CREATE TABLE IF NOT EXISTS `kelas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama_kelas` varchar(50) DEFAULT NULL,
@@ -42,7 +31,35 @@ CREATE TABLE IF NOT EXISTS `kelas` (
 ) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
--- Dumping structure for table test.kelas_mahasiswa
+-- Dumping structure for table AJKehadiran.mahasiswa
+DROP TABLE IF EXISTS `mahasiswa`;
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `nrp` varchar(14) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `angkatan` varchar(4) DEFAULT NULL,
+  `card_id` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`nrp`),
+  UNIQUE KEY `card_id` (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- Dumping structure for table AJKehadiran.kehadiran
+DROP TABLE IF EXISTS `kehadiran`;
+CREATE TABLE IF NOT EXISTS `kehadiran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_id` varchar(14) NOT NULL,
+  `class_id` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK_kehadiran_kelas` (`class_id`),
+  KEY `FK_kehadiran_mahasiswa` (`student_id`),
+  CONSTRAINT `FK_kehadiran_kelas` FOREIGN KEY (`class_id`) REFERENCES `kelas` (`id`),
+  CONSTRAINT `FK_kehadiran_mahasiswa` FOREIGN KEY (`student_id`) REFERENCES `mahasiswa` (`nrp`)
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table AJKehadiran.kelas_mahasiswa
+DROP TABLE IF EXISTS `kelas_mahasiswa`;
 CREATE TABLE IF NOT EXISTS `kelas_mahasiswa` (
   `student_id` varchar(14) DEFAULT NULL,
   `class_id` int(11) NOT NULL,
@@ -53,18 +70,16 @@ CREATE TABLE IF NOT EXISTS `kelas_mahasiswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
--- Dumping structure for table test.mahasiswa
-CREATE TABLE IF NOT EXISTS `mahasiswa` (
-  `nrp` varchar(14) NOT NULL,
-  `nama` varchar(50) DEFAULT NULL,
-  `angkatan` varchar(4) DEFAULT NULL,
-  `card_id` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`nrp`),
-  UNIQUE KEY `card_id` (`card_id`)
+-- Dumping structure for table AJKehadiran.last_scan
+DROP TABLE IF EXISTS `last_scan`;
+CREATE TABLE IF NOT EXISTS `last_scan` (
+  `id` int(11) DEFAULT NULL,
+  `cid` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
--- Dumping structure for table test.sessions
+-- Dumping structure for table AJKehadiran.sessions
+DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE IF NOT EXISTS `sessions` (
   `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   `expires` int(11) unsigned NOT NULL,
@@ -73,7 +88,8 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
--- Dumping structure for table test.users
+-- Dumping structure for table AJKehadiran.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) NOT NULL,
@@ -83,6 +99,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
+
+INSERT INTO last_scan(id, cid) VALUES(1,"test");
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
